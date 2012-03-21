5935f01ad5106eb950523d2546801fa03f2e711e
nanzhang <zhangnan@networking.io>
2012-03-21 13:50:21 +0800

    更新production.rb JS和CSS 编译

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Icolor::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
    config.assets.precompile += ['jquery.fancybox.js','jquery.js','LinkageSelect.js','location.js','main.js',
-                                'base.css','ie.css','news.css','reg.css','reset.css','style.css']
+                                'base.css','ie.css','news.css','reg.css','reset.css','style.css','index.css','jquery.masonry.js'
+                                ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
