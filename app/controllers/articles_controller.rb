32fd671a6a3eec8195d41f9bdaca151a4830b1ba
geekontheway <zxl51@qq.com>
2012-03-27 18:53:26 +0800

    ICOLOR-88 行业资讯功能完善;bump gem version;

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,16 @@
 class ArticlesController < ApplicationController
-
-  def index
-    @articles = Subject.content("articles").order("published_at DESC").page(params[:page]).per(5)  if Subject.content("articles")
-  end
+  before_filter :get_articles_with_tags
 
   def show
     @article = Post.find(params[:id])
+  end
+
+  def get_articles_with_tags
+    @tags = Subject.content("articles").tag_counts_on(:tags)
+    @articles = Subject.content("articles").order("published_at DESC").page(params[:page]).per(5) if Subject.content("articles")
+    if params[:tags]
+      @articles = @articles.tagged_with(params[:tags])
+    end
   end
 
 end
