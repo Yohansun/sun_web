a8d48538542e7785536b1d9d7c0041b3bcab6371
Dylan Zheng <hui@networking.io>
2012-04-01 16:50:48 +0800

    Seed subjects data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 Role.create([{ :role => 'designer' }, { :role => 'company' }, { :role => 'user' }])
+
+Subject.create(:slug => "weekly_tips", :content_type => "WeeklyTip")
+Subject.create(:slug => "articles", :content_type => "Post")
+Subject.create(:slug => "master_designs", :content_type => "MasterDesign")
+Subject.create(:slug => "master_profile", :content_type => "MasterProfile")
+Subject.create(:slug => "master_topics", :content_type => "Post")
