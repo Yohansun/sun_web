9894223967c6ae13197df74acda2d3aa9d877f62
mikegao <mike.gao0611@gmail.com>
2012-04-06 18:02:16 +0800

    ICOLOR-158,160 除了转发部分的其他开发工作

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -1,8 +1,8 @@
 class ChannelController < ApplicationController
   #片区快查
   def access  
-    @design_users = User.where("name like ? or username like ?", "%" + params[:keywords] + "%", "%" + params[:keywords] + "%")
-    @design_users = User.where(:area_id => params[:area_id]) unless params[:area_id].blank?
+    @design_users = User.where("name like ? or username like ?", "%" + params[:keywords].to_s + "%", "%" + params[:keywords].to_s + "%")
+    @design_users = @design_users.where(:area_id => params[:area_id]) unless params[:area_id].blank?
     @design_users = @design_users.where(:des_status => params[:des_status]) unless params[:des_status].blank?
     @design_users = @design_users.page(params[:page])
   end
