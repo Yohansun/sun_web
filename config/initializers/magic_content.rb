cfdc829c4fe68c4f26773e56ed18f6be7945b831
Dylan Zheng <hui@networking.io>
2012-04-06 10:53:33 +0800

    ICOLOR-173 扩展Post增加comments留言功能

--- config/initializers/magic_content.rb
+++ config/initializers/magic_content.rb
@@ -1,3 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 MagicContent.extra_content_options = {'大师殿堂' => 'MasterProfile', '一周装修小贴士' => 'WeeklyTip','大师作品' => 'MasterDesign'}
+
+Post.class_eval do
+  has_many :comments, :as => :commentable
+end
