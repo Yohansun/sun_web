03fb74f15ff5436490f09081a0ea04316a8f3c17
Dylan Zheng <hui@networking.io>
2012-04-07 17:40:54 +0800

    ICOLOR-175 6.1片区快查中顶部设计师地图快查需要建立后台，根据客户主推设计师定期更新设计师的功能，点击每个设计师头像可以进入其对应的设计师主页

--- config/initializers/magic_content.rb
+++ config/initializers/magic_content.rb
@@ -2,6 +2,10 @@
 
 MagicContent.extra_content_options = {'大师殿堂' => 'MasterProfile', '一周装修小贴士' => 'WeeklyTip','大师作品' => 'MasterDesign', '色彩搭配' => 'ColorDesign'}
 
+MagicContent.extra_settings = [
+    {:tag => 'text_field_tag', :key => 'promote_designers', :label => '片区快查推荐设计师'},
+]
+
 Post.class_eval do
   has_many :comments, :as => :commentable
 end
