eb7713d527afdba24911b29226643410681f6ba1
Dylan Zheng <hui@networking.io>
2012-04-07 15:25:28 +0800

    修正片区查看报错

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,7 +1,7 @@
 class Avatar < ActiveRecord::Base
   belongs_to :user
   has_attached_file :file,
-    :styles => {:upload => "155x135#", :profile => "194x194#", :icon => "48x48#", :thumb => "78x70#"},
+    :styles => {:upload => "155x135#", :profile => "194x194#", :icon => "48x48#", :thumb => "78x70#", :access_icon => "57x57#"},
     :whiny_thumbnails => true,
     :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension",
     :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:id.:extension"
