62a37db9a19e7c4011ec3f74719a17738d46fb88
nanzhang <zhangnan@networking.io>
2012-04-09 20:45:08 +0800

    ICOLOR-244:订阅电子期刊邮箱填写，如果首字符是数字就提示错误，类是QQ邮箱这样的邮箱地址都不能订阅

--- app/controllers/maillists_controller.rb
+++ app/controllers/maillists_controller.rb
@@ -1,8 +1,9 @@
 class MaillistsController < InheritedResources::Base
   def create
     create! do |succ, fail|
+      @valid_result = resource.errors.messages[:email][0]
       succ.js { render 'success' }
-      fail.js { render 'fail' }
+      fail.js { render 'fail', :locals => { :v => @valid_result } }
     end
   end
 end
--- app/models/maillist.rb
+++ app/models/maillist.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Maillist < ActiveRecord::Base
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_format_of :email, :with => /^([a-z][a-z0-9\_\.]*[a-z0-9])(@)(([a-z0-9][a-z0-9\-]*[a-z0-9][\.])+(com|cn|net|hk|tw|au|uk|de|tv|info|biz))$/i, :message => "格式错误"
+  validates_presence_of :email ,:message => "您输入的邮箱不能为空，请重新输入！"
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ , :message => "您输入的邮箱格式不正确，请重新输入！"
+  validates_uniqueness_of :email,:message => "您输入的邮箱已经被注册，请重新输入！"
 end
