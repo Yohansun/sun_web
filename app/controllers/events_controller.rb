5300549e99953a38138fa4b2f6b9f0839d7690d6
nanzhang <zhangnan@networking.io>
2012-04-12 16:41:33 +0800

    ICOLOR-161:分离用户主页公用部分以及修正活动公告板块

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,12 +32,34 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @event = current_user.events.where(:id => params[:id])
+    @event = current_user.events.find(params[:id])
+    render :action => "new"
+  end
+
+  def update
+    @event = current_user.events.find(params[:id])
+    if @event.update_attributes(params[:event])
+      redirect_to user_events_path
+    else
+      render :action => 'new'
+    end
   end
 
   def show
     @designers ||= MasterVideo.all
     @designer ||= MasterVideo.find(params[:id])
     @comments ||= Comment.where("commentable_id = ? and commentable_type = ?", params[:id], "MasterVideo").page(params[:page]).per(6)
+  end
+
+  def destroy
+    current_user.events.find(params[:id]).destroy
+    redirect_to user_events_path(current_user)
+  end
+
+  def my_event
+    if @user
+      @events = @user.events.page params[:page]
+      render :template => "events/users/my_event"
+    end
   end
 end
