20befde5d349e5910209a28c7198a614691a0908
mikegao <mike.gao0611@gmail.com>
2012-04-13 14:19:00 +0800

    首页动态关联数据

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,32 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
   def index
-    @user = User.new
     @weibo_data = WeiboData.where(:q => "放飞色彩心愿清单").where("thumbnail_pic IS NOT NULL").order("created_time DESC").limit(18).all
+
+    #每周之星
+    @week_star = WeeklyStar.first
+
+    #家装资讯
+    #@article = Subject.content("articles").first
+    #大师访谈
+    @master_interview = MasterProfile.first
+
+    #热点话题
+    @hot_topic = Post.first
+
+    #大师作品
+    @master_design = MasterDesign.first
+
+    #TODO灵感秀
+    @inspirations = ""
+
+    #推荐作品  TODO group_by 不知道数据多了以后翻页效果如何，待测试
+#    @design_groups = Design.select([:id, :shares_count]).group_by{|a| index = a.id%2==0}
+#    @designs_odd = @design_groups[false]
+#    @designs_even = @design_groups[true]
+    @designs = Design.all
+
+    #行业资讯
+    @articles = Subject.content("articles").limit(5)
   end
 end
