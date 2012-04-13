c05a5f3ff98232e2b05204e02f7b246babdcfd94
xiaoliang <xiaoliang@shawn-MBP.local>
2012-04-13 19:03:08 +0800

    ICOLOR-263，微博首页微博抓取数据的后台审核功能

--- app/models/weibo_item.rb
+++ app/models/weibo_item.rb
@@ -2,7 +2,7 @@
 
 require "open-uri"
 
-class WeiboData < ActiveRecord::Base
+class WeiboItem < ActiveRecord::Base
   # attr_accessible :title, :body
   validates_uniqueness_of :idstr
 
@@ -11,7 +11,7 @@ class WeiboData < ActiveRecord::Base
     data = open("http://api.weibo.com/2/search/topics.json?source=337220139&q=#{CGI.escape(q)}&count=50&page=#{page}").read
     lines = JSON.parse(data.force_encoding("UTF-8"))
     lines['statuses'].each do |status|
-      WeiboData.create(:q => q, :idstr => status['idstr'], :text => status['text'],
+      WeiboItem.create(:q => q, :idstr => status['idstr'], :text => status['text'],
         :source => status['source'], :thumbnail_pic => status['thumbnail_pic'],
         :bmiddle_pic => status['bmiddle_pic'], :original_pic => status['original_pic'],
         :user_id => status['user']['id'], :user_screen_name => status['user']['screen_name'],
