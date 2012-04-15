77b8a21b5f5c7744ebb0fbb5a174118e268bdeba
mikegao <mike.gao0611@gmail.com>
2012-04-15 22:17:19 +0800

    优化在线配色后台回复功能

--- app/controllers/magic_content/mix_colors_controller.rb
+++ app/controllers/magic_content/mix_colors_controller.rb
@@ -10,17 +10,16 @@ module MagicContent
     end
 
     def update
-      @mix = MixColor.where("id = ?", params[:id])
-      msg = "亲爱的#{@mix.to_a[0].user.display_name}您好！我们根据您提出的问题，设计了与您的家居与软装设计最适合的搭配风格供您参考，希望能给到您帮助！针对您问题里的“#{@mix.to_a[0].description}”，我们建议#{params[:customer_reply][:reply_content]}"
-      @reply = CustomerReply.create( :reply_content => msg,
-                            :reply_id => current_user.id,
+      @reply = CustomerReply.create(
+                            :reply_content => params[:customer_reply][:reply_content],
+                            #:reply_id => resource.user.id,
                             :question_id => params[:id],
                             :question_type => controller_name,
-                            :user_id => @mix.to_a[0].user_id
+                            :user_id => resource.user.id
                           )
       if @reply
         MixColor.update(params[:id], :status => 1)
-        redirect_to "/admin/content/mix_colors"
+        redirect_to main_app.mix_colors_path
       else
         render :action => "edit"
       end
