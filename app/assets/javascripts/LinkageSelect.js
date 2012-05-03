cc5843bb68f0746beb35022354ee845d79cb6590
Dylan Zheng <hui@networking.io>
2012-05-03 12:10:02 +0800

    ICOLOR-467 自定义LinkageSelect，可加入提示

--- app/assets/javascripts/LinkageSelect.js
+++ app/assets/javascripts/LinkageSelect.js
@@ -1,15 +1,15 @@
 /**
  * jQuery多级联动菜单
- * 
+ *
  * Author:	ZhouFan <happyeddie@gmail.com>
- * 
+ *
  */
 
 function LinkageSelect(options) {
-	
+
 	var bindEls	= new Array();
 	var items	= {};
-	
+
 	// 默认参数
 	var settings = {
 		data		: {},
@@ -20,73 +20,73 @@ function LinkageSelect(options) {
 		method		: 'post',
 		field_name	: null,
 		auto		: false
-	}; 
-	
+	};
+
 	// 自定义参数
-	if(options) {  
-		jQuery.extend(settings, options); 
+	if(options) {
+		jQuery.extend(settings, options);
 	}
-	
+
 	items	= settings.data;
-	
+
 	/**
 	 * 绑定元素
 	 * @param {Object} element
 	 * @param {Object} value
 	 */
-	function _bind(element , value) {
-		
+	function _bind(element , value, prompt) {
+
 		// 对象关联的key
 		var key	= bindEls.length ?
 			bindEls[bindEls.length - 1].key + ',' + bindEls[bindEls.length - 1].value :
 			settings.root;
-		
+
 		// 将绑定的元素放入数组
 		bindEls.push({
 			element	: element,
 			key		: key,
 			value	: value
 		});
-		
+
 		var item_count	= 0;
 		for (var i in items) {
 			item_count++;
 		}
-		
+
 		// 查找自身id
 		for (var el_id in bindEls) {
 			if (bindEls[el_id].element == element) {
 				var self_id	= parseInt(el_id);
 			}
 		}
-		
+
 		for(var el_id in bindEls){
 			// 为所有前面的对象增加onchange事件，onchange时，清空自身
 			if (el_id < self_id){
 				bindEls[el_id].element.change(function() {
-					_fill(element);
+					_fill(element, undefined, undefined, prompt);
 				})
 			}
 		}
-		
+
 		// 为上一级对象增加onchange事件，以刷新自身列表
 		if (self_id > 0) {
 			bindEls[self_id-1].element.change(function() {
-				_fill(element , bindEls[self_id].key);
+				_fill(element , bindEls[self_id].key, undefined, prompt);
 			});
 		}
-		
+
 		element.change(function() {
 			var self_key			= bindEls[self_id-1]?bindEls[self_id].key + ',' + $(this).val():'0,' + $(this).val();
 			if (typeof bindEls[self_id+1] != 'undefined') {
 				bindEls[self_id+1].key	= self_key;
 			}
-			
+
 			// 自动填入所选值
 			if (settings.field_name) {
 				$(settings.field_name).val($(this).val());
 			}
-			
+
 			if (settings.auto) {
 				if (typeof bindEls[self_id+1] == 'undefined') {
 					_find(self_key , function(key , json) {
@@ -94,31 +94,31 @@ function LinkageSelect(options) {
 							var el	= $('<select></select>');
 							element.after(el);
 							_bind(el , '');
-							_fill(bindEls[self_id+1].element , key , json);
+							_fill(bindEls[self_id+1].element , key , json, prompt);
 						}
 					});
 				}
 			}
 		})
-		_fill(element , key , value);
-		
+		_fill(element , key , value, prompt);
+
 	}
-	
+
 	/**
 	 * 填充option
 	 * @param {Object} element
 	 * @param {Object} key
 	 * @param {Object} value
 	 */
-	function _fill(element , key , value) {
-		
+	function _fill(element , key , value, prompt) {
+
 		element.empty();
-		element.append('<option value="">请选择</option>');
-		
+		element.append('<option value="">' + prompt + '</option>');
+
 		var json	= _find(key , function() {
 			_fill(element , key , value);
 		});
-		
+
 		if (!json) {
 			if (settings.auto)
 				element.hide();
@@ -138,7 +138,7 @@ function LinkageSelect(options) {
 			element.append(option);
 			index++;
 		}
-		
+
 		if (element[0]) {
 			//IE6
 			setTimeout(function(){
@@ -150,7 +150,7 @@ function LinkageSelect(options) {
 		}
 		element.width(element.width());
 	}
-	
+
 	/**
 	 * 查找元素
 	 * @param {Object} key
@@ -161,14 +161,14 @@ function LinkageSelect(options) {
 		} else if (key[key.length-1] == ',') {	// 若key以','结尾，肯定是取不到值
 			return null
 		} else if(typeof(items[key]) == "undefined") {
-			
+
 			// 计算items元素个数
 			var item_count	= 0;
 			for (var i in items) {
 				item_count++;
 				break;
 			}
-			
+
 			if (settings.ajax) {
 				$.getJSON(settings.ajax , {key:key} , function(json) {
 					items[key] = json;
@@ -181,10 +181,10 @@ function LinkageSelect(options) {
 				})
 			}
 		}
-			
+
 		return items[key];
 	}
-	
+
 	/**
 	 * 获取对象
 	 * @param {Object} element
@@ -196,19 +196,23 @@ function LinkageSelect(options) {
 			return element;
 		}
 	}
-	
-	
+
+
 	return {
-		
+
 		// 绑定元素
-		bind	: function(element , value) {
+		bind	: function(element , value, prompt) {
+			if (typeof(prompt) == 'undefined') {
+				prompt = '请选择';
+			};
+
 			if (typeof element != 'object')
 				element	= _getEl(element);
 			value	= value?value:'';
-			
-			_bind(element , value);
-			
+
+			_bind(element , value, prompt);
+
 		}
 	}
-	
+
 }
