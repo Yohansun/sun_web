48ddb12c5d642ba9dd7e49d39158fbc611bcae7f
mikegao <mike.gao0611@gmail.com>
2012-05-25 15:46:21 +0800

    ICOLOR-552

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,32 +1,28 @@
 class ScoresController < ApplicationController
 	def share_score
-		if current_user
-			score_type = 0
-			case params[:share_id]
-			when "tsina"
-				score_type = 501
-			when "qzone"
-				score_type = 500
-			when "renren"
-				score_type = 502
-			when "kaixin001"
-				score_type = 503
-			end
-			case params[:data_type]
-			when "Design"
-				Design.increment_counter(:shares_count, params[:data_id])
-			when "WeeklyStar"
-				WeeklyStar.increment_counter(:shares_count, params[:data_id])
-			when "Inspiration"
-				Inspiration.increment_counter(:shares_count, params[:data_id])
-			when "ColorDesign"
-				ColorDesign.increment_counter(:shares_count, params[:data_id])
-			end
-			current_user.share_score(current_user, score_type)
-			render :text => "ok"
-		else
-			render :text => "no user"
-		end		
+		score_type = 0
+		case params[:share_id]
+		when "tsina"
+			score_type = 501
+		when "qzone"
+			score_type = 500
+		when "renren"
+			score_type = 502
+		when "kaixin001"
+			score_type = 503
+		end
+		case params[:data_type]
+		when "Design"
+			Design.increment_counter(:shares_count, params[:data_id])
+		when "WeeklyStar"
+			WeeklyStar.increment_counter(:shares_count, params[:data_id])
+		when "Inspiration"
+			Inspiration.increment_counter(:shares_count, params[:data_id])
+		when "ColorDesign"
+			ColorDesign.increment_counter(:shares_count, params[:data_id])
+		end
+		current_user.share_score(current_user, score_type) if current_user
+		render :text => "score ok"
 	end
 
 end
