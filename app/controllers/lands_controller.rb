d5b89f87e04aaa5e6da2d539122de7b5e2edac12
Dylan Zheng <hui@networking.io>
2012-06-11 16:00:42 +0800

    Revert "ICOLOR-605 移除掉landing统计代码，改由piwik统计"
    This reverts commit b7e80575fab885e4dcb4d345eb1fbc4635ff1624.

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class LandsController < ApplicationController
 	def landing
-		# Land.create(:source => params[:source], :source_ip => request.remote_ip )
-		redirect_to root_path(:landing_source => params[:source])
+		Land.create(:source => params[:source], :source_ip => request.remote_ip )
+		redirect_to root_path
 	end
 end
