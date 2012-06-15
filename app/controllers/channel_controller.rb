d5482dcdaa2f1f16f5f6a0412693920873be5c2e
ZhangNan <zhangnan@networking.io>
2012-06-15 14:22:59 +0800

    ICOLOR-642:修改片区快查查询逻辑

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -3,7 +3,7 @@ class ChannelController < ApplicationController
 
   #片区快查
   def access
-    #排序规则：先每周之星，次作品有色号，后上传作品数
+    #排序规则：本周之星 -> 往期之星 -> 作品有色号 -> 上传作品数
     unless params[:keywords] == "请输入关键字"
       @design_users = User.where("name like ? or username like ?", "%#{params[:keywords]}%", "%#{params[:keywords]}%")
     else
@@ -19,7 +19,7 @@ class ChannelController < ApplicationController
     unless params[:city_id].blank?
       areas = []
       Area.find(params[:city_id]).children.map{|a| areas << a.id}
-      @design_users = @design_users.where("area_id in (#{areas.join(",")})")      
+      @design_users = @design_users.where("area_id in (#{areas.join(",")})")
     end
 
     unless params[:area_id].blank?
@@ -37,19 +37,27 @@ class ChannelController < ApplicationController
     user_url = WeeklyStar.order("published_at desc").select("author_url")
     ws = []
     user_url.each do |ul|
-      s = ul.author_url.match(%r(http://www.icolor.com.cn/users/(\d{1,5})?)).to_a[1]
+      s = ul.author_url.match(%r(http://www.icolor.com.cn/users/(\d{1,})?)).to_a[1]
       ws << s unless s.blank?
     end
-    cons = []
-    week_now = user_url.first.author_url.match(%r(http://www.icolor.com.cn/users/(\d{1,5})?)).to_a[1]
-    unless @design_users.where("id = (#{week_now})").blank?
-      cons << "id = (#{week_now}) desc"
-    else
-      cons << "id in (#{ws.join(",")}) desc"
-    end
 
+    cons = []
+    #本周之星 -> 往期之星
+    if !ws.blank? && params[:user_role] != "company"
+      cons << "id = #{ws.first} desc"
+      ws.delete(ws.first)
+      cons << "id in (#{ws.join(",")}) desc"
+    elsif params[:user_role] == "company"
+      #根据后台基础设置中推荐家装公司的ID进行排序
+      unless MagicSetting.recommend_designers.blank?
+       cons << "id in (#{MagicSetting.recommend_designers}) desc"
+      end
+    end
+    #输入立邦色号的数量
     cons << "recommend_designer_status desc"
+    #上传作品数量
     cons << "designs_count desc"
+
     @design_users = @design_users.order(cons.join(","))
     @design_users = @design_users.page(params[:page]).per(9)
   end
