9544cc023dc336fe46c3d426b2cb9ddccfa0c549
Dylan Zheng <hui@networking.io>
2012-06-26 17:09:03 +0800

    ICOLOR-682 每周数据导出

--- app/controllers/magic_admin/users_controller.rb
+++ app/controllers/magic_admin/users_controller.rb
@@ -2,10 +2,14 @@
 module MagicAdmin
   class UsersController < BaseController
     def index
-    	@users = User.select("id, username, name, email, phone, role_id, des_status").page(params[:page]).per(20)
     	if params[:search]
-    	   @users = User.search(params)
-    	end
-    end 
+        @from = Time.new(params[:search]['created_at_from(1i)'],
+          params[:search]['created_at_from(2i)'], params[:search]['created_at_from(3i)'])
+        @to = Time.new(params[:search]['created_at_to(1i)'],
+          params[:search]['created_at_to(2i)'], params[:search]['created_at_to(3i)'])
+
+        @users = User.where("created_at >= '#{@from}' AND created_at <= '#{@to}'")
+      end
+    end
   end
 end
