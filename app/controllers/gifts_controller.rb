953987af73280aa7503f10dc26fa4cda59aaf110
alikewmk <wang@networking.io>
2012-07-17 13:30:44 +0800

    ICOLOR-777 获奖名单逻辑修改

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -10,19 +10,22 @@ class GiftsController < ApplicationController
 		@gift1 = Gift.where(" gift_type like ?", "iColor点评王奖").order("give_time DESC")
 		@gift2 = Gift.where(" gift_type like ?", "月度最佳色彩及设计奖").order("give_time DESC")
 		@gift3 = Gift.where(" gift_type like ?", "注册幸运奖").order("give_time DESC")
-		@gift4 = Gift.where(" gift_type like ?", "投票有礼奖").order("give_time DESC")		
-	end
+		@gift4 = Gift.where(" gift_type like ?", "投票有礼奖").order("give_time DESC")
+    end
+       
 
 	def get_dates_1
-    	@dates_arr1 = []
-    	@gift1 = Gift.where(" gift_type like ?", "iColor点评王奖").order("give_time DESC")
-     	@gift1.each do |g| 
-     		time = g.give_time.strftime("%Y/%m/%d").to_s + ','+ g.end_time.strftime("%Y/%m/%d").to_s
-     		unless @dates_arr1.include? time
-     			@dates_arr1 << time 	
-     		end		
-		end
-		@dates_arr1
+         @dates_arr1 = []              
+         @gift1 = Gift.where(" gift_type like ?", "iColor点评王奖").order("give_time DESC")
+         unless @gift1.nil?
+             @gift1.each do |g|
+              time = [(g.give_time - 1.days), (g.end_time - 1.days)]
+                unless @dates_arr1.include? time
+                    @dates_arr1 << time     
+                end  
+             end
+         @dates_arr1
+         end
 	end
 
 	def get_dates_2
@@ -30,7 +33,7 @@ class GiftsController < ApplicationController
     	@gift2 = Gift.where(" gift_type like ?", "月度最佳色彩及设计奖").order("give_time DESC")
     	unless @gift2.nil?
      	@gift2.each do |g|  
-     		time = g.give_time.strftime("%Y/%m").to_s + ','+ g.end_time.strftime("%Y/%m").to_s
+     		time = g.end_time.strftime("%Y/%m")
      		unless @dates_arr2.include? time	
      			@dates_arr2 << time 	
      		end		
@@ -40,17 +43,17 @@ class GiftsController < ApplicationController
 	end
 
 	def get_dates_3
-    	@dates_arr3 = []
-    	@gift3 = Gift.where(" gift_type like ?", "注册幸运奖").order("give_time DESC")
-    	unless @gift3.nil?
-     	@gift3.each do |g| 
-     		time = g.give_time.strftime("%Y/%m/%d").to_s + ','+ g.end_time.strftime("%Y/%m/%d").to_s
-     		unless @dates_arr3.include? time	
-     			@dates_arr3 << time 	
-     		end		
-		end
-		@dates_arr3
-	    end
+    	 @dates_arr3 = []              
+         @gift3 = Gift.where(" gift_type like ?", "注册幸运奖").order("give_time DESC")
+         unless @gift3.nil?
+             @gift3.each do |g|
+              time = [(g.give_time - 1.days), (g.end_time - 1.days)]
+                unless @dates_arr3.include? time
+                    @dates_arr3 << time     
+                end  
+             end
+         @dates_arr3
+         end
 	end
 
 	def get_dates_4
@@ -58,7 +61,7 @@ class GiftsController < ApplicationController
     	@gift4 = Gift.where(" gift_type like ?", "投票有礼奖").order("give_time DESC")	
     	unless @gift4.nil?
      	@gift4.each do |g|
-     		time = g.give_time.strftime("%Y/%m").to_s + ','+ g.end_time.strftime("%Y/%m").to_s
+     		time = g.end_time.strftime("%Y/%m")
      		unless @dates_arr4.include? time	
      			@dates_arr4 << time 	
      		end		
