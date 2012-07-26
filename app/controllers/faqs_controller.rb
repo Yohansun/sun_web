c9d6f95bec4786eaa272088e3c1752591cb0e497
ZhangNan <zhangnan@networking.io>
2012-07-26 14:33:10 +0800

    ICOLOR-814:添加‘家装设计咨询’--我要提问 错误信息提示

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,10 +1,11 @@
+# -*- encoding : utf-8 -*-
 class FaqsController < ApplicationController
   before_filter :find_user
 
   def index
     if @user
       @faqs = @user.faqs.page(params[:page]).per(5)
-      #@faqs = @user.customer_replies.page(params[:page]).per(5)
+
       render :template => "#{controller_name}/users/#{action_name}"
     else
       @faqs = Faq.page(params[:page]) || Faq.new
@@ -21,7 +22,17 @@ class FaqsController < ApplicationController
     @faq = current_user.faqs.create(params[:faq])
     if @faq.save
       current_user.create_score(current_user.id, 1001, 1, 10)
-      redirect_to faqs_path
+    else
+      errors = ""
+      @faq.errors.messages.each do |key, value|
+        key = key.eql?(:title) ? "标题:" : "内容:"
+
+        errors << "*" + key + value[0] + '\n'
+      end 
+
+      flash[:errors] = errors
     end
+
+    redirect_to faqs_path
   end
 end
