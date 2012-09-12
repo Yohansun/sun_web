4dfbe4f0a949caeb42a79a08bfdb7a75c053c5b6
liugang <501225658@qq.com>
2012-09-12 19:08:58 +0800

    [#Icolor]关于我们,网站声明部署

--- config/routes.rb
+++ config/routes.rb
@@ -119,6 +119,7 @@ Icolor::Application.routes.draw do
   match "/rules" => "function#rules" #会员机制页面
   match "/rules2" => "function#rules2" #话费积分活动细则
   match "/state" => "function#state" #网站声明
+  match "/about" => "function#about" #关于我们
   match "/suc_ins" => "function#suc_ins" #发布成功页面
   match "/suc_works" => "function#suc_works" #发布成功页面
   match "/token" => "function#token" #头像修改页面
