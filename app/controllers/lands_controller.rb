03dfa04dabc391c5051e8974db658f14d75ccdee
ZhangNan <zhangnan@networking.io>
2012-09-29 11:20:47 +0800

    修改10.1活动land跳转链接

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -2,6 +2,25 @@
 class LandsController < ApplicationController
 	def landing
 		Land.create(:source => params[:source], :source_ip => request.remote_ip )
-		redirect_to root_path
+
+		#10.1活动
+		link = %w(
+			taipingyangshouye
+			taipingyangshejishishouye
+			taipingyangshejishiwenzhang
+			taipingyangzhuangshi
+			taipingyangbankuaidingzhi
+			zhuangxiulunbo
+			zhuangxiubannner
+			zhuangxiuketing
+			zhuangxiuwuoshi
+			zhuangxiuertong
+			zhuangxiusecai)
+
+		if link.include? params[:source]
+			redirect_to national_day_path
+		else
+			redirect_to root_path
+		end
 	end
 end
