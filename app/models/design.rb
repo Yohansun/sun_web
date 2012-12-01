60f04fd27f584634afe51e7ba391e38fb32eb856
hui <hui@networking.io>
2012-12-02 00:20:55 +0800

    cache cover_img

--- app/models/design.rb
+++ app/models/design.rb
@@ -5,14 +5,14 @@ class Design < ActiveRecord::Base
   acts_as_taggable
 
   attr_accessible :city, :content, :room_type, :style, :title, :design_image_ids,
-    :tag_list, :area_id, :reason, :user_id, :view_count, :recommended, :design_color, 
+    :tag_list, :area_id, :reason, :user_id, :view_count, :recommended, :design_color,
     :recommend_color_category1, :is_yda, :is_refresh
   validates_presence_of :title, :content, :reason, :design_color, :area_id
 
   belongs_to :user
   has_many :comments, :as => :commentable
   has_many :votes, :as => :voteable
-  has_many :design_images, :as => :imageable, :dependent => :delete_all
+  has_many :design_images, :as => :imageable, :dependent => :delete_all, :order => 'is_cover DESC'
   has_many :color_codes
 
   paginates_per 8
@@ -27,7 +27,7 @@ class Design < ActiveRecord::Base
   end
 
   def cover_img
-    self.design_images.order("is_cover DESC").first
+    self.design_images.first
   end
 
   def comments_count
