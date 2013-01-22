2718e8dc8973e4a0fe5ce8bbdc09b5a7d215f8ac
liugang <501225658@qq.com>
2013-01-22 16:38:36 +0800

    [IColor]修改标签设置的AJAX

--- app/models/image_tag.rb
+++ app/models/image_tag.rb
@@ -1,3 +1,5 @@
 class ImageTag < ActiveRecord::Base
   attr_accessible :design_image_id, :image_library_category_id
+
+  scope :existed, lambda { |image_id, cid| where(["design_image_id = ? and image_library_category_id = ?", image_id, cid]) unless image_id.nil? && cid.nil? }
 end
