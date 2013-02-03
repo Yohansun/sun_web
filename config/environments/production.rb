3d125fd997902badee638db8b268440a4fbb1be3
hui <hui@networking.io>
2013-02-03 22:28:37 +0800

    disable exception email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Icolor::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.middleware.use ExceptionNotifier,
-    :email_prefix => "[ICOLOR] ",
-    :sender_address => %{"errors" <errors@networking.io>},
-    :exception_recipients => %w{errors@networking.io}
+  # config.middleware.use ExceptionNotifier,
+  #   :email_prefix => "[ICOLOR] ",
+  #   :sender_address => %{"errors" <errors@networking.io>},
+  #   :exception_recipients => %w{errors@networking.io}
 end
