ab3f1c4dca83df401acf4c76fce94e3073a7cf42
liugang <501225658@qq.com>
2013-02-17 17:34:32 +0800

    [IColor]首页图片,kv优化

--- app/models/weekly_star_upload.rb
+++ app/models/weekly_star_upload.rb
@@ -1,6 +1,7 @@
 class WeeklyStarUpload < ActiveRecord::Base
   belongs_to :weekly_stars
   has_attached_file :file, :styles => {:thumb => "105x97#", :slide => "550x365#", :slide_thumb => "205x138#",
+  																		 :home_page1 => "280x365#",
                                        :fullscreen => "980x655>", :fullscreen_thumb => "100x120#"},
                     :whiny_thumbnails => true,
                     :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension",
