2c649e70202aecf209d939440b39662450c7cc37
liugang <501225658@qq.com>
2013-02-28 18:24:26 +0800

    [IColor]添加监测

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -30,9 +30,38 @@ class LandsController < ApplicationController
 
 			urls3 =["80018secaidapei2",
 							"pchousebankuaidingzhi2",
-							"pchousebankuaidingzhi-2"]
+							"pchousebankuaidingzhi-2",
+						  "souhu_jiajushouye",
+							"souhu_jiajushejishi",
+							"souhu_tuliaopindao",
+							"souhu_xinwenzhongxin",
+							"souhu_bendixinwen",
+							"hejia_shouye",
+							"hejia_youqituliaohangye",
+							"hejia_jushangzhongduan",
+							"hejia_tuwenzixunzhongduan",
+							"80018_secaidapei_neiyeyouce",
+							"80018_shejishi_neiyeyouce"]
 
 			urls4 = ["a963shouyejiaodian1","a963shouyeredian1","a963shejishitongdao1"]
+
+			urls_design_images = [
+							"soufang_quanguo_pinpai",
+							"soufang_xinwen_pinpai",
+							"soufang_pinpailuntan_zhutie",
+							"soufang_beijin_shouye",
+							"soufang_shanghai_shouye",
+							"soufang_guangzhou_shouye",
+							"soufang_shenzheng_shouye",
+							"soufang_tianjing_shouye",
+							"soufang_chongqing_shouye",
+							"soufang_chengdu_shouye",
+							"soufang_wuhan_shouye",
+							"soufang_hangzhou_shouye",
+							"soufang_shijiazhuang_shouye",
+							"soufang_xiaoquwang_pinpaizhanshi",
+							"souhu_zhuangxiudaxue_shouye",
+							"80018_wangyouyangbanjian_neiyeyouce"]
 
 			if urls.include? params[:source]
 				redirect_to home_refresh_path
@@ -42,6 +71,8 @@ class LandsController < ApplicationController
 				redirect_to root_path
 			elsif urls4.include? params[:source]
 				redirect_to gift_list_path
+			elsif urls_design_images.include? params[:source]
+				redirect_to design_images_path
 			else
 				redirect_to root_path
 			end
