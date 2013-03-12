d1ab5c92a8ad2493bb7b74a42ab38835e34813eb
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-03-12 19:03:04 +0800

    套图id出现错误问题错误图的问题

--- app/models/design_image.rb
+++ app/models/design_image.rb
@@ -137,7 +137,7 @@ class DesignImage < ActiveRecord::Base
       when 'no_edit_color'
         DesignImage.available.where("(design_images.edited_color = ? or design_images.edited_color is null) AND (design_images.audited is null or design_images.audited = ?)", false, false).order("design_images.id DESC")
         when 'imageable_id'
-        DesignImage.available.where("design_images.imageable_id = ?", keyword).order("design_images.id DESC")
+        DesignImage.available.where("design_images.imageable_id = ? and design_images.imageable_type = ?", keyword, 'MasterDesign').order("design_images.id DESC")
     end
   end
 
