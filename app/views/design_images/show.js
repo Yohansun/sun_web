c1cebfeb39760640820710cd4eb2109644a2e0f3
yukui <yukui@networking.io>
2013-03-19 14:00:41 +0800

    灵感秀无法上传作品

--- app/views/design_images/show.js
+++ app/views/design_images/show.js
@@ -1,6 +1,8 @@
 $('#uploads').append("<%= escape_javascript(render(:partial => "upload", :locals => {:upload => @upload})) %>");
 $('.<%= params[:for] %>_images_size').html(parseInt($('.<%= params[:for] %>_images_size').html()) + 1);
-$('#image_tag').append("<%= escape_javascript(render(:partial => "img_lib_tag", :locals => {:img_lib_tag => @upload})) %>");
+<% if params[:controller] == "inspirations" %>
+	$('#image_tag').append("<%= escape_javascript(render(:partial => "img_lib_tag", :locals => {:img_lib_tag => @upload})) %>");
+<% end %>
 var txt = parseInt($('.work_size').last().text());
 var work_size = $('.load_total span').text();
 $('.load_total span').text(work_size + txt);
