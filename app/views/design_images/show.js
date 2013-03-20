0cf6290ea43bea409c4463118351a4b1a06b8ddb
Sky <sky@localhost.(none)>
2013-03-20 16:07:48 +0800

    灵感秀上传和作品上传修改 上传结果页修改

--- app/views/design_images/show.js
+++ app/views/design_images/show.js
@@ -1,6 +1,6 @@
 $('#uploads').append("<%= escape_javascript(render(:partial => "upload", :locals => {:upload => @upload})) %>");
 $('.<%= params[:for] %>_images_size').html(parseInt($('.<%= params[:for] %>_images_size').html()) + 1);
-<% if params[:controller] == "inspirations" %>
+<% if @upload.imageable_type == "Design" %>
 	$('#image_tag').append("<%= escape_javascript(render(:partial => "img_lib_tag", :locals => {:img_lib_tag => @upload})) %>");
 <% end %>
 var txt = parseInt($('.work_size').last().text());
