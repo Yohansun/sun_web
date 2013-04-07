bf90bb6099b444fc6a0824c436474e43f8d81473
yukui <yukui@networking.io>
2013-04-07 19:42:12 +0800

    修复link_to有img_tag标签导至alt标签不封闭图片不能显示Bug

--- config/initializers/rails_ext.rb
+++ config/initializers/rails_ext.rb
@@ -1,8 +1,10 @@
 module ActionView::Helpers::UrlHelper
 	
   def link_to_with_title(name, options = {}, html_options = nil)
-    html_options = {:title => name} if html_options.nil?
-    html_options[:title] = name if html_options[:title].nil?
+  	unless name =~ /<img/
+	    html_options = {:title => name} if html_options.nil?
+	    html_options[:title] = name if html_options[:title].nil?
+	end
     link_to_without_title(name, options, html_options)
   end
 
