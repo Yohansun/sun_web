6cc4f35a293c687a8de7bebae646692ef61b21ab
yukui <yukui@networking.io>
2013-04-07 20:12:14 +0800

    修复link_to do 包含img_tag导至图片不能显示Bug

--- config/initializers/rails_ext.rb
+++ config/initializers/rails_ext.rb
@@ -1,11 +1,11 @@
 module ActionView::Helpers::UrlHelper
 	
-  def link_to_with_title(name, options = {}, html_options = nil)
+  def link_to_with_title(name, options = {}, html_options = nil, &block)
   	unless name =~ /<img/
 	    html_options = {:title => name} if html_options.nil?
 	    html_options[:title] = name if html_options[:title].nil?
 	end
-    link_to_without_title(name, options, html_options)
+    link_to_without_title(name, options, html_options, &block)
   end
 
   alias_method_chain :link_to, :title
