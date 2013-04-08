c3fd56d4f0641dd1d9fb5bb73474c2fb492a3881
hui <hui@networking.io>
2013-04-08 22:42:11 +0800

    注释掉assets相关部署代码，防止出错。

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,16 +20,16 @@ set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock co
 namespace :deploy do
 
   # COMMET BELOW WHEN FRESH CAP
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
+  # namespace :assets do
+  #   task :precompile, :roles => :web, :except => { :no_release => true } do
+  #     from = source.next_revision(current_revision)
+  #     if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  #     else
+  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
+  #     end
+  #   end
+  # end
 
   task :start, :roles => :app do
     unicorn.start
