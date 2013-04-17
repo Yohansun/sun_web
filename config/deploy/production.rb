cf77f2692482137a770e30cf20181f526e79b7bd
liugang <501225658@qq.com>
2013-04-17 15:33:38 +0800

    BUG #235 4.17 灵感秀内页投票数显示问题,sitemap部署生成

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -52,7 +52,7 @@ namespace :deploy do
 end
 
 before 'bundle:install', 'deploy:symlink_shared'
-after "deploy", "refresh_sitemaps", "update_crontab"
+after "deploy:update","refresh_sitemaps","update_crontab"
 
 require 'capistrano-unicorn'
 
