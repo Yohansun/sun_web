05b9e99f2422bbf7b42ce3d47a99862a57fc106d
zhangming <zhangming@networking.io>
2013-05-17 18:09:50 +0800

    BUG #301

--- app/models/design_image.rb
+++ app/models/design_image.rb
@@ -164,7 +164,7 @@ class DesignImage < ActiveRecord::Base
       when 'no_edit_color'
         DesignImage.available.where("(design_images.edited_color = ? or design_images.edited_color is null) AND (design_images.audited is null or design_images.audited = ?)", false, false).order("design_images.id DESC")
       when 'imageable_id'
-        DesignImage.available.where("design_images.imageable_id = ? and design_images.imageable_type = ?", keyword, 'MasterDesign').order("design_images.id DESC")
+        DesignImage.available.where("design_images.imageable_id = ?", keyword).order("design_images.id DESC")
       when 'no_audited'
         DesignImage.available.where("no_audited is true").order("design_images.id DESC")
       when 'pub_time'
