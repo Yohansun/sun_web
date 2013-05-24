76efc66623a1de73c85330376bf293ef0807aaf2
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-05-24 14:46:18 +0800

    时尚转身页面 和 系统声明

--- app/controllers/baicheng/fashion_model_controller.rb
+++ app/controllers/baicheng/fashion_model_controller.rb
@@ -2,10 +2,14 @@
 class Baicheng::FashionModelController < ApplicationController
   layout 'baicheng'
   before_filter :find_user
+
   def index
     @title = "时尚榜样_装修设计作品活动主页-立邦 iColor 装修设计鉴赏、设计师作品欣赏、访谈"
     @description = "立邦icolor因爱之名刷新生活 时尚榜样平台，您可以看到设计师上传的设计作品及已选择房型平面图,更多家装需求，等待您的设计。"
     @key_words = '立邦刷新生活,装修设计作品,房型平面图'
+  end
+  
+  def show
   end
 
 end
