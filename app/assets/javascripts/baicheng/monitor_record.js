395820eb8c0f407c92bb213fbeaf639d90f43bee
zhangming <zhangming@networking.io>
2013-05-25 01:41:46 +0800

    增加 点击 坚实代码

--- app/assets/javascripts/baicheng/monitor_record.js
+++ app/assets/javascripts/baicheng/monitor_record.js
@@ -160,7 +160,24 @@ function init_monitor_link_click() {
         });
          triggerEBConversion(326801);
     });
+<<<<<<< HEAD
  
+=======
+
+    $('#monitor_link_326802').click(function() {
+        CClicki[10008]._trackEvent({
+            type: 1,
+            labels: [
+                {"按钮名称": "登陆"}
+            ],
+            values: [
+                {"数量": 1}
+            ]
+        });
+         triggerEBConversion(326802);
+    });
+
+>>>>>>> 增加 点击 坚实代码
     $('#monitor_link_326803').click(function() {
         CClicki[10008]._trackEvent({
             type: 1,
