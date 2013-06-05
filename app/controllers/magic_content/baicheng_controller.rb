d1bd75b4c945c1902beb52aa9d7ad8c8ed19c76f
zhangming <zhangming@networking.io>
2013-06-05 14:56:28 +0800

    add query

--- app/controllers/magic_content/baicheng_controller.rb
+++ app/controllers/magic_content/baicheng_controller.rb
@@ -3,6 +3,19 @@ module MagicContent
 
     def daily_report
       
+      case params[:search_type]  
+      when 'yesterday'
+        begin_t = Time.now.yesterday.beginning_of_day
+        end_t = Time.now.yesterday.end_of_day
+      when 'custom'
+        begin_t = Time. params[:begin_date]
+          
+        end_t =  params[:end_date]
+      else 
+        begin_t = Time.now.beginning_of_day
+        end_t = Time.now.end_of_day
+      end
+      @results = Story.where(created_at: begin_t..end_t).page(params[:page])
     end
   end
 end
