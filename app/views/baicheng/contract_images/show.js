4dde6d99934c30d4be333f2d7dac14c038d182ab
zhangming <zhangming@networking.io>
2013-06-21 17:48:25 +0800

    upload contract

--- app/views/baicheng/contract_images/show.js
+++ app/views/baicheng/contract_images/show.js
@@ -1,2 +1,2 @@
 $('#uploads').append("<%= escape_javascript(render(:partial => "upload", :locals => {:upload => @upload})) %>");
-$('.contract_image_size').html(parseInt($('.contract_image_size').html()) + 1);
+ 
