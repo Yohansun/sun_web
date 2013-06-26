dd1c853ac8bdb4cb069da1e3b8de5c489cd77a5c
liugang <501225658@qq.com>
2013-06-26 17:33:50 +0800

    添加房型图用户的删除依赖

--- app/models/story.rb
+++ app/models/story.rb
@@ -16,7 +16,7 @@ class Story < ActiveRecord::Base
   has_one :baicheng_event
   has_one :contract
   # has_many :story_comments, :dependent => :destroy #如果这句用不着,就删了吧
-  has_many :story_users
+  has_many :story_users, :dependent => :destroy
   has_many :want_designers, :through => :story_users, :source => :user #想设计的设计师
   # has_many :tags, class_name: 'StoryImageTag', :foreign_key => 'story_image_id',:dependent => :destroy
 
