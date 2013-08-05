e2c67e810910333399e14320ae06286766d74cd7
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-08-05 17:02:53 +0800

    csm::上传图片不能显示

--- app/models/fit_image.rb
+++ app/models/fit_image.rb
@@ -3,7 +3,7 @@ class FitImage < ActiveRecord::Base
   attr_accessible :title, :link, :file
 
   has_attached_file :file,
-    :styles => {:img => "138x165>"},
+    :styles => {:thumb => "60x45#", :img => "138x165>"},
     :whiny_thumbnails => true,
     :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension",
     :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:id.:extension"
--- app/models/home_banner.rb
+++ app/models/home_banner.rb
@@ -4,7 +4,7 @@ class HomeBanner < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :file,
-    :styles => {:chief => "270x100>",:owner_enter =>"290x200>", :bottom_big => "670x85>", :bottome_little => "310x85>", :master => "1000x85>"},
+    :styles => {:thumb => "60x45#", :chief => "270x100>",:owner_enter =>"290x200>", :bottom_big => "670x85>", :bottome_little => "310x85>", :master => "1000x85>"},
     :whiny_thumbnails => true,
     :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension",
     :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:id.:extension"
