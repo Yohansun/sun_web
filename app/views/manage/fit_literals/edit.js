ad4d26228144c6ed6d0be9f15783ee697889700e
yukui <yukui@networking.io>
2013-08-15 15:06:27 +0800

    装修资讯-文字资讯修改性提交

--- app/views/manage/fit_literals/edit.js
+++ app/views/manage/fit_literals/edit.js
@@ -10,4 +10,14 @@ $(".literal_submit").click(function(){
 });
 $(".inspect_fit_liter_link").click(function(){
     window.open($(".liter_link").val());
-})
+});
+$('.literal_submit').click(function(){
+	var $position = $('.literal_select').val();
+	var $title = $('.literal_title').val();
+	var $url = $('.liter_link').val();
+	$.ajax({
+	  type: "PUT",
+	  url: "<%= fit_literal_path %>",
+	  data: {fit_literal_id : <%= @new.id %>, order_id : $position, title : $title, link : $url}
+	});
+});
