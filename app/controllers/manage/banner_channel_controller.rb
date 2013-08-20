504d2e8deac670d976e0d0eb0f489bef53101f90
liugang <501225658@qq.com>
2013-08-20 16:34:52 +0800

    修改设计快查文字标识

--- app/controllers/manage/banner_channel_controller.rb
+++ app/controllers/manage/banner_channel_controller.rb
@@ -6,19 +6,19 @@ class Manage::BannerChannelController < Manage::BaseController
   	@i_banner_groups_count = @i_banner_groups.count
 
   	if params[:position]
-  		@i_banner = IBanner.page_name('业主通道').position(params[:position]).first
+  		@i_banner = IBanner.page_name('设计快查').position(params[:position]).first
   	else
-  		@i_banner = IBanner.page_name('业主通道').first
+  		@i_banner = IBanner.page_name('设计快查').first
   	end
   end
 
   def create_image
   	position =  params[:position]
-    @i_banner = IBanner.page_name('业主通道').position(position).first
+    @i_banner = IBanner.page_name('设计快查').position(position).first
     result = "上传失败"
     if @i_banner
   		@i_banner.file = params[:file].tempfile if params[:file].present?
-  		@i_banner.page_name = '业主通道'
+  		@i_banner.page_name = '设计快查'
     	if @i_banner.save
     		if params[:file].present?
   				result = "上传成功"
@@ -34,7 +34,7 @@ class Manage::BannerChannelController < Manage::BaseController
   	photo_name = params[:photo_name]
   	i_banner_group_id = params[:i_banner_group_id]
 
-  	@i_banner = IBanner.page_name('业主通道').position(position).first
+  	@i_banner = IBanner.page_name('设计快查').position(position).first
   	if @i_banner
   		@i_banner.photo_name = photo_name
   		@i_banner.i_banner_group_id = i_banner_group_id
--- db/migrate/20130820071144_add_init_to_i_banners.rb
+++ db/migrate/20130820071144_add_init_to_i_banners.rb
@@ -1,8 +1,8 @@
 #encoding: utf-8
 class AddInitToIBanners < ActiveRecord::Migration
   def change
-  	IBanner.create(page_name: "业主通道", position: 1, size: '1000x85')
-  	IBanner.create(page_name: "业主通道", position: 2, size: '670x85')
-  	IBanner.create(page_name: "业主通道", position: 3, size: '310x85')
+  	IBanner.create(page_name: "设计快查", position: 1, size: '1000x85')
+  	IBanner.create(page_name: "设计快查", position: 2, size: '670x85')
+  	IBanner.create(page_name: "设计快查", position: 3, size: '310x85')
   end
 end
