2bb363499948e13970272c9469a6e1acd8ad8a23
sky <yuanxiaofei_apache@163.com>
2013-08-23 11:34:08 +0800

    设计快查样式修改

--- app/controllers/manage/channel_tips_controller.rb
+++ app/controllers/manage/channel_tips_controller.rb
@@ -6,13 +6,20 @@ class Manage::ChannelTipsController < Manage::BaseController
   end
 
   def update_tip
+    rank = 0
     @channel_tip = ChannelTip.find(params[:id])
+    rank = @channel_tip.rank
     @channel_tip.title = params[:title]
     @channel_tip.rank = params[:rank]
     @channel_tip.content = params[:content]
     @channel_tip.link = params[:link]
     @channel_tip.admin_id = current_admin.id
     if @channel_tip.save
+      unless rank.to_i == params[:rank].to_i
+        ct = ChannelTip.where("rank = ? and updated_at < ?",@channel_tip.rank,@channel_tip.updated_at).first
+        ct.rank = rank
+        ct.save
+      end
       render js: "alert('保存成功!');location.reload();"
     else
       render js: "alert('保存失败!');"      
