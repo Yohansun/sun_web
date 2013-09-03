2af2783597ef541cbfbb93c18e11efe01335ee75
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-09-03 17:51:32 +0800

    新版 刷新更多，通知栏后台修改

--- app/controllers/refresh_life/base_refresh_controller.rb
+++ app/controllers/refresh_life/base_refresh_controller.rb
@@ -3,6 +3,9 @@ class RefreshLife::BaseRefreshController < ApplicationController
 	before_filter :get_data
 
   def get_data
+  	@design_more = IColumnData.where(i_column_type_id: 2, position: 0).first
+  	@weekly_star_more = IColumnData.where(i_column_type_id: 3, position: 0).first
+  	@master_design_more = IColumnData.where(i_column_type_id: 1, position: 0).first
     @design_datas = IColumnData.where(i_column_type_id: 2).limit 5
     @weekly_star_datas = IColumnData.where(i_column_type_id: 3).limit 5
     @master_design_datas = IColumnData.where(i_column_type_id: 1).limit 5
