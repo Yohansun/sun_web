4a0b0bd0cd8777b4e7c76c7e192897c8bcdd0dc3
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-09-10 14:37:32 +0800

    TASK #2254 9.9 刷新生活精彩资讯列表CMS 逻辑修改/bug 图库首页图片头条插入一行修改，刷新效果修改

--- app/controllers/refresh_life/refresh_list_controller.rb
+++ app/controllers/refresh_life/refresh_list_controller.rb
@@ -3,7 +3,7 @@ class RefreshLife::RefreshListController < RefreshLife::BaseRefreshController
   #caches_action :index, :expires_in => 30.minutes, :cache_path => Proc.new { |c| c.params }
 
   def index
-    @i_life_infos = ILifeInfo.order("updated_at desc").page(params[:page]).per(5)
+    @i_life_infos = Post.where(subject_id: 1).order("updated_at desc").page(params[:page]).per(5)
     if @i_life_infos
       if @i_life_infos.count % 5 == 0
         @page_count = @i_life_infos.count / 5
@@ -11,6 +11,10 @@ class RefreshLife::RefreshListController < RefreshLife::BaseRefreshController
         @page_count = (@i_life_infos.count / 5).to_i + 1
       end
     end
+  end
+
+  def show
+    @article = Post.find params[:id]
   end
 
 end
