ad021be4155f9430c6c9a92cb973d4b56447882a
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-09-12 14:36:49 +0800

    刷新效果底步banner修改/刷新效果替换艺术漆地铁秀界面

--- app/controllers/refresh_life/base_refresh_controller.rb
+++ app/controllers/refresh_life/base_refresh_controller.rb
@@ -7,9 +7,10 @@ class RefreshLife::BaseRefreshController < ApplicationController
   	@design_more = IColumnData.where(i_column_type_id: 2, position: 0).first
   	@weekly_star_more = IColumnData.where(i_column_type_id: 3, position: 0).first
   	@master_design_more = IColumnData.where(i_column_type_id: 1, position: 0).first
-    @design_datas = IColumnData.where(i_column_type_id: 2).limit 5
-    @weekly_star_datas = IColumnData.where(i_column_type_id: 3).limit 5
-    @master_design_datas = IColumnData.where(i_column_type_id: 1).limit 5
+    @design_datas = IColumnData.where(i_column_type_id: 2).limit(6).order("rank asc")
+    @weekly_star_datas = IColumnData.where(i_column_type_id: 3).limit(6).order("rank asc")
+    @master_design_datas = IColumnData.where(i_column_type_id: 1).limit(6).order("rank asc")
+    @wonderful_datas = IColumnData.where(i_column_type_id: 17).limit(6).order("rank asc")
     @master_design_data_url = IColumnData.where(i_column_type_id: 1).first
     @i_banner = IBanner.page_name("刷新实录")
   end
