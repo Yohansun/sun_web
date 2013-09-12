4430d3cba5596f3c01cd5f4050829596609ebb61
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-09-12 16:19:19 +0800

    精华推荐精彩咨询名称修改

--- app/controllers/refresh_life/base_refresh_controller.rb
+++ app/controllers/refresh_life/base_refresh_controller.rb
@@ -10,7 +10,7 @@ class RefreshLife::BaseRefreshController < ApplicationController
     @design_datas = IColumnData.where(i_column_type_id: 2).limit(6).order("rank asc")
     @weekly_star_datas = IColumnData.where(i_column_type_id: 3).limit(6).order("rank asc")
     @master_design_datas = IColumnData.where(i_column_type_id: 1).limit(6).order("rank asc")
-    @wonderful_datas = IColumnData.where(i_column_type_id: 17).limit(6).order("rank asc")
+    @wonderful_datas = IColumnData.where(i_column_type_id: 16).limit(6).order("rank asc")
     @master_design_data_url = IColumnData.where(i_column_type_id: 1).first
     @i_banner = IBanner.page_name("刷新实录")
   end
