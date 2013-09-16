c7dd00dd9931f30b8af89095873e65d7229ddbe6
liugang <501225658@qq.com>
2013-09-16 12:15:19 +0800

    修改样式

--- app/controllers/refresh_life/base_refresh_controller.rb
+++ app/controllers/refresh_life/base_refresh_controller.rb
@@ -9,9 +9,11 @@ class RefreshLife::BaseRefreshController < ApplicationController
   	@master_design_more = IColumnData.where(i_column_type_id: 1, position: 0).first
     @design_datas = IColumnData.where(i_column_type_id: 2).limit(6).order("rank asc")
     @weekly_star_datas = IColumnData.where(i_column_type_id: 3).limit(6).order("rank asc")
-    @master_design_datas = IColumnData.where(i_column_type_id: 1).limit(6).order("rank asc")
+    #精彩资讯 通栏逻辑
+    @master_design_datas = IColumnData.show_data(1).limit(5)
+    @master_design_data_url = IColumnData.where(i_column_type_id: 1, position: 0).first
+
     @wonderful_datas = IColumnData.where(i_column_type_id: 17).limit(6).order("rank asc")
-    @master_design_data_url = IColumnData.where(i_column_type_id: 1).first
     @i_banner = IBanner.page_name("刷新实录")
   end
 
