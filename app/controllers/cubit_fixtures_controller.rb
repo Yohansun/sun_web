98c39e8596c601d603aba0a435f9418d2b2329d6
sky <yuanxiaofei_apache@163.com>
2013-09-24 15:44:28 +0800

    刷新效果页面的通栏修改，通栏CMS中将标题限制改为16个字，设计快查和装修图库内页的装修丘比特发送邮件格式错误

--- app/controllers/cubit_fixtures_controller.rb
+++ app/controllers/cubit_fixtures_controller.rb
@@ -2,14 +2,16 @@
 class CubitFixturesController < ApplicationController
 
   def create
-    if params[:cubit_fixture]
-      fixtue = CubitFixture.new
-        if fixtue.update_attributes(params[:cubit_fixture])
-          Notifier.cubit_fixture(fixtue.id).deliver
-          render :js => "alert('谢谢您的参与！ 丘比特将在3个工作日内向您推荐3家同城优秀家装公司！');location.reload();"
-        else
-          render :js => "alert('*号为必填项,所在地必须到区!');"
-        end
+    if params[:cubit_fixtures]
+      fixtue = CubitFixture.new(params[:cubit_fixtures])
+      if fixtue.save
+        Notifier.cubit_fixture(fixtue.id).deliver
+        render :js => "alert('谢谢您的参与！ 丘比特将在3个工作日内向您推荐3家同城优秀家装公司！');location.reload();"
+      else
+        render :js => "alert('*号为必填项,所在地必须到区!');"
+      end
+    else
+      render :js => "alert('*号为必填项,所在地必须到区!');"
     end
   end
 end
