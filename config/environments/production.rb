3ab9d2c32d2bca3f169ff1ddccb9ce886fe217cb
hui <hui@networking.io>
2013-10-30 11:52:38 +0800

    TASK #2652 优化图库页面打开速度 - 打开多通道素材加载

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Icolor::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
 
-  if Socket.gethostname == "icolor"
+  if Socket.gethostname == "ICOLOR-WEB"
     config.action_controller.asset_host = Proc.new { |source|
       "http://icolorimg#{Digest::MD5.hexdigest(source).to_i(16) % 3 + 1}.networking.io"
     }
