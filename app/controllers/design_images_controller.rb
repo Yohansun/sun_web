dd938a0035f4b688a8c22523d392f5bb6454ec1f
hui <hui@networking.io>
2013-12-05 15:32:01 +0800

    缓存图库列表页

--- app/controllers/design_images_controller.rb
+++ app/controllers/design_images_controller.rb
@@ -6,8 +6,8 @@ class DesignImagesController < ApplicationController
   caches_action :image_show, :cache_path => Proc.new { |c|
     image_show_design_image_path(c.params.delete_if{|k,v| k == 'path'})
   }, :expires_in => 7.days
-
   caches_action :lists, :cache_path => Proc.new { |c| c.params }, :expires_in => 7.days
+  caches_action :index, :cache_path => Proc.new { |c| c.params }, :expires_in => 7.days
 
   def create
     newparams = coerce(params)
