516138052a370d3ef4cad746754504802e2807d1
qy <qinyin@networking.io>
2013-12-27 17:23:29 +0800

    icolor设计鉴赏页面添加分享按钮

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -717,9 +717,6 @@ $(function(){
    //$(this).siblings('div.popup_zf').toggle();
 
   })
-  $('.zhuanfa').hover(function(){
-    $(this).next('.popup_zf').toggle();
-  });
 
   $('.forward,.trans a').hover(function(){
    //$(this).siblings('div.popup_zf').toggle();
