63a4a16eeafde78275db1ff0fcf8ed677a40db30
yoshikizh <177365340@qq.com>
2014-02-17 10:54:06 +0800

    dialog celebrity media

--- app/models/celebrity_question_image.rb
+++ app/models/celebrity_question_image.rb
@@ -2,4 +2,9 @@ class CelebrityQuestionImage < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :image
   belongs_to :resource, polymorphic: true
+
+  has_attached_file :image, :styles => { :thumb => "64x64#",:slide => "640x480>" },
+    :whiny_thumbnails => true,
+    :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension",
+    :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:id.:extension"
 end
