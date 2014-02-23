c15613bc81066cfb444d762ae9c19cfafa6cd32d
yoshikizh <177365340@qq.com>
2014-02-23 17:18:02 +0800

    dialog celebrity

--- app/controllers/dialog_celebrity/master_designs_controller.rb
+++ app/controllers/dialog_celebrity/master_designs_controller.rb
@@ -2,12 +2,13 @@ class DialogCelebrity::MasterDesignsController < ApplicationController
   def show
     @master_design = MasterDesign.find(params[:id])
     @upload = MasterDesignUpload.find(params[:upload_id])
+    @master = @master_design.master_profile
   end
 
   def download
     upload = MasterDesignUpload.find(params[:id])
     if upload
-      send_file upload.file.path(:slide)
+      send_file upload.file2.path(:slide)
     else
       render nothing: true, status: 404
     end
