671ec53c21d20bcf0007ca8bf7f2e892cbbb8ebc
qy <qinyin@networking.io>
2014-02-24 17:58:06 +0800

    STORY #2711 2.21 对话名人提问页面调整（完成日期：2.24）；colorbox效果；和上图图片缩略图样式；

--- app/assets/javascripts/famous.js
+++ app/assets/javascripts/famous.js
@@ -11,23 +11,23 @@
     $(this).next().find('.js-text_maxlenth').text(maxlenth);
   });
   //
-  $('.famous_ask_form').on('click','.js-famous_options',function(){
-    $(this).siblings('.select_options').show();
-  });
-  $('.famous_ask_form').on('click','.js-selects_close',function(){
-    $(this).parent('.select_options').hide();
-  });
-  //famous ask select option
-  $('.select_options')
-  .on('change','select',function () {
-    var str = '';
-    var this_index = $(this).index('select');
-    $(this).children('option:selected').each(function() {
-      str = $( this ).text();
-    });
-    $('.select_options_show').children().eq(this_index - 1).text(str);
-  })
-  .change();
+  // $('.famous_ask_form').on('click','.js-famous_options',function(){
+  //   $(this).siblings('.select_options').show();
+  // });
+  // $('.famous_ask_form').on('click','.js-selects_close',function(){
+  //   $(this).parent('.select_options').hide();
+  // });
+  // //famous ask select option
+  // $('.select_options')
+  // .on('change','select',function () {
+  //   var str = '';
+  //   var this_index = $(this).index('select');
+  //   $(this).children('option:selected').each(function() {
+  //     str = $( this ).text();
+  //   });
+  //   $('.select_options_show').children().eq(this_index - 1).text(str);
+  // })
+  // .change();
   //submit
   $('.js-famous_ask_submit').on('click',function(){
     var overview_val = $('.famous_ask_form').find('.js-text_percentage').val();
