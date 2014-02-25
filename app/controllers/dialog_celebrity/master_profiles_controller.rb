498affdf5d0cecf990ac7bc84acca606fe4c6da8
yoshikizh <177365340@qq.com>
2014-02-25 22:47:05 +0800

    dialogy celebrity

--- app/controllers/dialog_celebrity/master_profiles_controller.rb
+++ app/controllers/dialog_celebrity/master_profiles_controller.rb
@@ -3,26 +3,26 @@ class DialogCelebrity::MasterProfilesController < ApplicationController
   include CommonModule
   before_filter :get_page_data,:only => [:celebrities,:maste_interviews]
   def celebrities
-    @celebrities = MasterProfile.where(mtype: 1).order("id desc")
+    @celebrities = MasterProfile.where(mtype: 1).order("updated_at desc")
     @banners = IBanner.page_name('名人在线').order("position ASC")
     @new_designs = MasterDesign.order("updated_at desc").limit(5)
   end
 
   def maste_interviews
-    @masters = MasterProfile.where(mtype: 0)
+    @masters = MasterProfile.unscoped.where(mtype: 0)
     if params[:board_id].present?
       @masters = @masters.where(:celebrity_content_board_id => params[:board_id].to_i)
     end
     if params[:tag].present?
       @masters = @masters.where(:nationality => params[:tag])
     end
-    @masters = @masters.order("id desc").page(params[:page]).per(8)
+    @masters = @masters.order("created_at desc").page(params[:page]).per(8)
     @banners = IBanner.page_name('名家设计').order("position ASC")
   end
 
   def master_interview
     @master = MasterProfile.find(params[:id])
-    @last_masters = MasterProfile.order("created_at desc").limit(4)
+    @last_masters = MasterProfile.unscoped.order("created_at desc").limit(4)
     @banners = IBanner.page_name('名人名家访谈页').order("position ASC")
   end
 end
