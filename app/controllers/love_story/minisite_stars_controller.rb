011cc249039725d6a7929d6bb3b44256277d9111
liugang <501225658@qq.com>
2014-03-05 11:06:47 +0800

    remove cache_action in minisite_star_controller

--- app/controllers/love_story/minisite_stars_controller.rb
+++ app/controllers/love_story/minisite_stars_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class LoveStory::MinisiteStarsController < LoveStory::BaseController
-  caches_action :index, :expires_in => 7.days
+  # caches_action :index, :expires_in => 7.days
   def index
     @users = User.where("top50 = ?",true).page(params[:page]).per(24)
   end
