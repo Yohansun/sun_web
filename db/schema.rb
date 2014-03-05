a4093b45469bab582e3139313653690fa5900f01
yoshikizh <177365340@qq.com>
2014-03-06 00:54:02 +0800

    dialog celbrity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140302111841) do
+ActiveRecord::Schema.define(:version => 20140305154733) do
 
   create_table "admin_profiles", :force => true do |t|
     t.integer  "admin_id"
@@ -1258,8 +1258,10 @@ ActiveRecord::Schema.define(:version => 20140302111841) do
     t.string   "username"
     t.string   "password"
     t.text     "boards"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.string   "email",      :default => ""
+    t.string   "mobile",     :default => ""
   end
 
   create_table "messages", :force => true do |t|
