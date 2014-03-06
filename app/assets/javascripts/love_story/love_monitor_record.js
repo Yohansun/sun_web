de92ea5ec32cc083303c07280b42b0a9d6de7056
liugang <501225658@qq.com>
2014-03-06 10:33:50 +0800

    合并Conversion Tag监测JS,解决冲突问题

--- app/assets/javascripts/love_story/love_monitor_record.js
+++ app/assets/javascripts/love_story/love_monitor_record.js
@@ -13,30 +13,6 @@ function newTriggerEBConversion(object, id) {
 };
 
 function init_monitor_link_click() {
-    // 为爱刷新_2首页_分享爱的故事
-    $('#love_monitor_link_436277_nologin').click(function() {
-        if (confirm("登录网站后才能分享爱的故事,是否现在登录?")) {
-            return newTriggerEBConversion(this, 436277);
-        }else{
-        return false;
-        };
-    });
-    // 为爱刷新_2首页_分享爱的故事
-    $('#love_monitor_link_436277_login').click(function() {
-        return newTriggerEBConversion(this, 436277);
-    });
-    // 为爱刷新_3首页_上传设计作品
-    $('#love_monitor_link_436278_nologin').click(function() {
-        if (confirm("登录网站后才能上传设计作品,是否现在登录?")) {
-            return newTriggerEBConversion(this, 436278);
-        }else{
-        return false;
-        };
-    });
-    // 为爱刷新_3首页_上传设计作品
-    $('#love_monitor_link_436278_login').click(function() {
-        return newTriggerEBConversion(this, 436278);
-    });
     // 为爱刷新_4首页_登陆
     $('#love_monitor_link_436279').click(function() {
         return newTriggerEBConversion(this, 436279);
@@ -48,18 +24,6 @@ function init_monitor_link_click() {
     // 为爱刷新_7爱的故事_定制我的私人设计师
     $('.love_monitor_link_436282 > map[name=story_kv] area').click(function() {
         return newTriggerEBConversion(this, 436282);
-    });
-    // 为爱刷新_8爱的故事_分享爱的故事
-    $('#love_monitor_link_436283_nologin').click(function() {
-        if (confirm("登录网站后才能分享爱的故事,是否现在登录?")) {
-        return newTriggerEBConversion(this, 436283);
-        }else{
-        return false;
-        };
-    });
-    // 为爱刷新_8爱的故事_分享爱的故事
-    $('#love_monitor_link_436283_login').click(function() {
-        return newTriggerEBConversion(this, 436283);
     });
     // 为爱刷新_10发布我的故事
     $('#love_monitor_link_436285').click(function() {
