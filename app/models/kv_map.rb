8214183e680856b2c998973c92e6dd56949fd318
liugang <501225658@qq.com>
2014-03-08 10:46:12 +0800

    添加数字验证

--- app/models/kv_map.rb
+++ app/models/kv_map.rb
@@ -4,4 +4,6 @@ class KvMap < ActiveRecord::Base
 
   belongs_to :home_kv
   belongs_to :event_kv
+  validates_presence_of :width, :high, :x_line, :y_line, message: '坐标值不能为空!'
+  validates_numericality_of :width, :high, :x_line, :y_line, :only_integer => true
 end
