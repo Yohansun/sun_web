c9e34096617f3b6ebb2d5fa3fb8f73a847cb8a2a
sky <yuanxiaofei_apache@163.com>
2014-03-12 15:19:35 +0800

    刷新效果页面通栏替换

--- app/controllers/refresh_life/refresh_show_controller.rb
+++ app/controllers/refresh_life/refresh_show_controller.rb
@@ -5,6 +5,8 @@ class RefreshLife::RefreshShowController < RefreshLife::BaseRefreshController
     @i_banner1 = IBanner.page_name("刷新效果").position(1).first
     @i_banner2 = IBanner.page_name("刷新效果").position(2).first
     @i_banner3 = IBanner.page_name("刷新效果").position(3).first
+    @wonderfuls = IColumnData.show_data(16).limit(5)
+    @wonderfuls_url = IColumnData.where(i_column_type_id: 16, position: 0).first
     expires_in 60.minutes, 'max-stale' => 2.hours, :public => true
   end
 end
